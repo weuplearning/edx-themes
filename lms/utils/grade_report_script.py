c0b7ff94f6f17f1ce1d5a847c4a9b1838ff5fea3
cyril-adolf-weup <cyril.adolf@weuplearning.com>
2024-04-18 13:54:09 +0000

    19068 - Bug on Progress bar for old course , deal with nwew course

--- lms/utils/grade_report_script.py
+++ lms/utils/grade_report_script.py
@@ -32,7 +32,7 @@ from openpyxl.styles import PatternFill, Font
 from opaque_keys.edx.locator import CourseLocator
 from common.djangoapps.student.models import CourseEnrollment
 from lms.djangoapps.courseware.courses import get_course_by_id
-from lms.djangoapps.wul_apps.best_grade.helpers import check_best_grade
+# from lms.djangoapps.wul_apps.best_grade.helpers import check_best_grade
 from lms.djangoapps.wul_apps.models import WulCourseEnrollment
 from lms.djangoapps.courseware.models import StudentModule
 
@@ -53,6 +53,66 @@ course_ids = sys.argv[2].split(";")
 
 all_users_data = {}
 
+umn_scorm_list = {
+  'course-v1:umn+test+test' : {
+    "umn_scorm_list_1" : [
+      'block-v1:umn+test+test+type@scorm+block@fd4f25a734734497bc0111f6fd549a26', 
+      'block-v1:umn+test+test+type@scorm+block@5fae2b299b0e47a48e8112d3b11aa615', 
+      'block-v1:umn+test+test+type@scorm+block@dc3e413d33234a1dafd0bca06ae0c5e1', 
+      'block-v1:umn+test+test+type@scorm+block@02915ef6d75747d1a04d87689a60542d', 
+      'block-v1:umn+test+test+type@scorm+block@b320c1c95f1f47cd8a9f63a441766f9a'
+    ],
+    "umn_scorm_list_2" : [
+      'block-v1:umn+test+test+type@scorm+block@f67c1876fa8c46e49f37596711c1768b', 
+      'block-v1:umn+test+test+type@scorm+block@cede2c08a2b24c09aeeab327522c1b09', 
+      'block-v1:umn+test+test+type@scorm+block@67c2292f054f4ad68702733b411dc6ca', 
+      'block-v1:umn+test+test+type@scorm+block@21c4ca65a2d54879bf843db10f1842f3',
+    ],
+    "umn_scorm_list_3" : [
+      'block-v1:umn+test+test+type@scorm+block@4e5ac4aba3cc4466b909ccc2246edc9a', 
+      'block-v1:umn+test+test+type@scorm+block@e618a3a9f7374586bb24ce31b0fd06bf', 
+      'block-v1:umn+test+test+type@scorm+block@0513ab9c8ac7434787e95b8e1085a549', 
+      'block-v1:umn+test+test+type@scorm+block@def1b70b68e4422cbe52694e68b3afa6', 
+      'block-v1:umn+test+test+type@scorm+block@9d02fb2863954a3d95b867ee0e066c7c', 
+      'block-v1:umn+test+test+type@scorm+block@72922a0c6c054283b8e554baffa121d0', 
+      'block-v1:umn+test+test+type@scorm+block@c347c583b97c45748d8e96674dce61ba'
+    ],
+    "umn_scorm_list_4" : [      
+      'block-v1:umn+test+test+type@scorm+block@5d1cf5d6f4794bc1ad8821efee50c85b',
+      'block-v1:umn+test+test+type@scorm+block@daa34dbada1441268b2106bfcf91817b'
+    ]
+  }, 
+  'course-v1:umn+pn+02' : {
+    "umn_scorm_list_1" : [
+      'block-v1:umn+pn+02+type@scorm+block@2cad17b664064248ac8a93388a464612',
+      'block-v1:umn+pn+02+type@scorm+block@bf9933dc1f2f452690f6b90f82326bc6',
+      'block-v1:umn+pn+02+type@scorm+block@d9686f98cf07492887129ad3fc4e64e5',
+      'block-v1:umn+pn+02+type@scorm+block@fef1daca9a794568bd76f761a5b992d1',
+      'block-v1:umn+pn+02+type@scorm+block@5080c48db29844728b1887355cc09159'
+    ],
+    "umn_scorm_list_2" : [
+      'block-v1:umn+pn+02+type@scorm+block@730855c5f22c4857adce5831ce323ebb',
+      'block-v1:umn+pn+02+type@scorm+block@3f16d65d916e40f0a9ee75a40ed8c30d',
+      'block-v1:umn+pn+02+type@scorm+block@6871056a6ee145819c109152413790ba',
+      'block-v1:umn+pn+02+type@scorm+block@d30a3b24ce5f48a4a4b90499841c2a5b'
+    ],
+    "umn_scorm_list_3" : [
+      'block-v1:umn+pn+02+type@scorm+block@4e5ac4aba3cc4466b909ccc2246edc9a',
+      'block-v1:umn+pn+02+type@scorm+block@e618a3a9f7374586bb24ce31b0fd06bf',
+      'block-v1:umn+pn+02+type@scorm+block@0513ab9c8ac7434787e95b8e1085a549',
+      'block-v1:umn+pn+02+type@scorm+block@df86fd4c446c4923a3dd7b77a01d932b',
+      'block-v1:umn+pn+02+type@scorm+block@aabd0c9d5afc4095bdc17fc1fdb2c17b',
+      'block-v1:umn+pn+02+type@scorm+block@d296cb76795a4ed79caa5384171507df',
+      'block-v1:umn+pn+02+type@scorm+block@c347c583b97c45748d8e96674dce61ba',
+    ],
+    "umn_scorm_list_4" : [
+      'block-v1:umn+pn+02+type@scorm+block@912617e856a24a0f8fb6537485bae663',
+      'block-v1:umn+pn+02+type@scorm+block@572e16ef99d94d5899bd0ce028c052d0'
+    ]
+  }
+}
+
+
 
 for course_id in course_ids:
   course_key = CourseLocator.from_string(course_id)
@@ -70,11 +130,6 @@ for course_id in course_ids:
     if str(user.email).find('@yopmail') != -1 or str(user.email).find('@weuplearning') != -1 or str(user.email).find('@themoocagency') != -1 :
       continue
 
-    # try:
-    #   user_data["id"] = user.id
-    # except:
-    #   user_data["id"] = 'n.a.'
-
     try:
       user_data["email"] = user.email
     except:
@@ -82,8 +137,6 @@ for course_id in course_ids:
         user_data["email"] = json.loads(user.profile.custom_field)['email']
       except:
         user_data["email"] = 'n.a.'
-    
-    # user_data["name"] = user.profile.name
 
     try:
       user_data["date_joined"] = user.date_joined.strftime('%Y-%m-%d %H:%M:%S')
@@ -141,40 +194,12 @@ for course_id in course_ids:
     list_of_student_modules = StudentModule.objects.filter(course_id__exact=course_id, module_type__exact="scorm", student_id=user.id).order_by().values('module_state_key', 'state')
     grade_list = []
 
-    umn_test_test_scorm_list = {
-      "umn_test_test_scorm_list_1" : [
-        'block-v1:umn+test+test+type@scorm+block@fd4f25a734734497bc0111f6fd549a26', 
-        'block-v1:umn+test+test+type@scorm+block@5fae2b299b0e47a48e8112d3b11aa615', 
-        'block-v1:umn+test+test+type@scorm+block@dc3e413d33234a1dafd0bca06ae0c5e1', 
-        'block-v1:umn+test+test+type@scorm+block@02915ef6d75747d1a04d87689a60542d', 
-        'block-v1:umn+test+test+type@scorm+block@b320c1c95f1f47cd8a9f63a441766f9a'
-      ],
-      "umn_test_test_scorm_list_2" : [
-        'block-v1:umn+test+test+type@scorm+block@f67c1876fa8c46e49f37596711c1768b', 
-        'block-v1:umn+test+test+type@scorm+block@cede2c08a2b24c09aeeab327522c1b09', 
-        'block-v1:umn+test+test+type@scorm+block@67c2292f054f4ad68702733b411dc6ca', 
-        'block-v1:umn+test+test+type@scorm+block@21c4ca65a2d54879bf843db10f1842f3'
-      ],
-      "umn_test_test_scorm_list_3" : [
-        'block-v1:umn+test+test+type@scorm+block@4e5ac4aba3cc4466b909ccc2246edc9a', 
-        'block-v1:umn+test+test+type@scorm+block@e618a3a9f7374586bb24ce31b0fd06bf', 
-        'block-v1:umn+test+test+type@scorm+block@0513ab9c8ac7434787e95b8e1085a549', 
-        'block-v1:umn+test+test+type@scorm+block@def1b70b68e4422cbe52694e68b3afa6', 
-        'block-v1:umn+test+test+type@scorm+block@9d02fb2863954a3d95b867ee0e066c7c', 
-        'block-v1:umn+test+test+type@scorm+block@72922a0c6c054283b8e554baffa121d0', 
-        'block-v1:umn+test+test+type@scorm+block@c347c583b97c45748d8e96674dce61ba'
-      ],
-      "umn_test_test_scorm_list_4" : [      
-        'block-v1:umn+test+test+type@scorm+block@5d1cf5d6f4794bc1ad8821efee50c85b',
-        'block-v1:umn+test+test+type@scorm+block@daa34dbada1441268b2106bfcf91817b'
-      ]
-    }
 
     final_grade_score = 0
-    for module in umn_test_test_scorm_list :
+    for module in umn_scorm_list[course_id] :
       total_module_score = 0
 
-      for scorm_id in umn_test_test_scorm_list[module] :
+      for scorm_id in umn_scorm_list[course_id][module] :
         module_score = 'n.a.'
 
         for scorm in list_of_student_modules:
@@ -190,16 +215,16 @@ for course_id in course_ids:
 
         grade_list.append(module_score)
 
-      module_average = round(total_module_score / len(umn_test_test_scorm_list[module]), 2)
+      module_average = round(total_module_score / len(umn_scorm_list[course_id][module]), 2)
       final_grade_score += module_average
       grade_list.append(module_average)
 
-    final_grade = round(final_grade_score / len(umn_test_test_scorm_list), 2)
+    final_grade = round(final_grade_score / len(umn_scorm_list[course_id]), 2)
     grade_list.append(final_grade)
 
 
-    user_data['grade_list'] = grade_list 
 
+    user_data['grade_list'] = grade_list 
     data = { "general": user_data }
     course_data[str(user.id)]= data
 
@@ -303,7 +328,4 @@ for email in emails:
 log.info('------------> Finish calculate grades and write xlsx report')
 
 
-# /edx/app/edxapp/venvs/edxapp/bin/python /edx/app/edxapp/edx-themes/umn/lms/utils/grade_report_script.py 'cyril.adolf@weuplearning.com' course-v1:umn+test+test
-
-
-
+# /edx/app/edxapp/venvs/edxapp/bin/python /edx/app/edxapp/edx-themes/umn/lms/utils/grade_report_script.py 'cyril.adolf@weuplearning.com' 'course-v1:umn+test+test;course-v1:umn+pn+02'
