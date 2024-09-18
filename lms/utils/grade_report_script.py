d9193ffd5d1046fe678d3691310305f8fccaa56f
dimitri-hoareau-WEL <dimitri.hoareau@weuplearning.com>
2024-09-18 11:46:48 +0000

    update grade report script to include tt and certificate date

--- lms/utils/grade_report_script.py
+++ lms/utils/grade_report_script.py
@@ -21,25 +21,18 @@ application = get_wsgi_application()
 #############################################################
 
 import csv 
-
 import json
 import time
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill, Font
 
-
-# from datetime import datetime, date, timedelta
-# from django.utils import timezone
-# from dateutil import tz
-
-
 from opaque_keys.edx.locator import CourseLocator
 from common.djangoapps.student.models import CourseEnrollment
 from lms.djangoapps.courseware.courses import get_course_by_id
 from lms.djangoapps.wul_apps.best_grade.helpers import check_best_grade
+from lms.djangoapps.wul_apps.models import WulCourseEnrollment
 
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
-
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -53,7 +46,6 @@ log = logging.getLogger()
 
 
 
-
 emails = sys.argv[1].split(";")
 course_ids = sys.argv[2].split(";")
 
@@ -62,30 +54,23 @@ org = "af-brasil"
 register_form = configuration_helpers.get_value_for_org(org, 'FORM_EXTRA')
 
 # Get headers
-HEADERS_GLOBAL = []
-HEADERS_USER = [u"ID", u"Email", u"Nom complet"]
-
+HEADERS_USER = [u"Nom complet", u"Email",u"Username", u"Phone",u"Nearest AF", u"Registration date", u"Last login"]
 HEADERS_FORM = []
-NICE_HEADERS_FORM = []
-
 
 
 if register_form is not None:
   for row in register_form:
     if row.get('type') is not None:
       HEADERS_FORM.append(row.get('name'))
-      NICE_HEADERS_FORM.append(row.get('label'))
 
-
-NICE_HEADER = list(NICE_HEADERS_FORM)
 TECHNICAL_HEADER = list(HEADERS_FORM)
 
-HEADERS_USER.extend(NICE_HEADER)
-HEADERS_USER.append('Note globale (en %)')
+UserGrade = ['CFL1','DFL1','CFL2','DFL2','CFL3','DFL3','CFL4','DFL4','CFL5','DFL5','CFL6','DFL6','CFL7','DFL7','CFL8','DFL8','CFL9','DFL9','CFL10','DFL10','CFL11','DFL11','CFL12','DFL12','CFL13','DFL13','CFL14','DFL14','CFL15','DFL15']
 
 
 if course_ids[0] == 'course-v1:af-brasil+PP+CPB' :
   HEADERS_SECTION = ['Quiz Primeiros Passos', 'Quiz Destination Paris', 'Quiz Apresentações', 'Quiz Tour Eiffel & Champ de Mars', 'Quiz Família & Pets', 'Quiz Château de Versailles', 'Quiz Tempo', 'Quiz Stade de France', 'Quiz Festas & Tradições', 'Quiz Yvelines', 'Quiz Estudos', 'Quiz Seine-Saint-Denis', 'Quiz Trabalho', 'Quiz Paris La Défense Arena', 'Quiz Lazer', 'Quiz Stades en France', 'Quiz Saúde', 'Quiz Invalides & Pont d\'Iéna', 'Quiz Viagem', 'Quiz Arenas Paris Sud', 'Quiz Cidade', 'Quiz Ailleurs en France', 'Quiz Casa', 'Quiz La Concorde', 'Quiz Gastronomia', 'Quiz Arena Bercy', 'Quiz Moda', 'Quiz Grand Palais', 'Quiz DELF A1', 'Quiz Arena Porte de La Chapelle']
+
 elif course_ids[0] == 'course-v1:af-brasil+PP+CPB01' :
   HEADERS_SECTION = ['Quiz Primeiros Passos', 'Quiz Destination Paris', 'Quiz Apresentações', 'Quiz Tour Eiffel & Champ de Mars', 'Quiz Família & Pets', 'Quiz Château de Versailles', 'Quiz Tempo', 'Quiz Stade de France', 'Quiz Festas & Tradições', 'Quiz Yvelines', 'Quiz Estudos', 'Quiz Seine-Saint-Denis', 'Quiz Trabalho', 'Quiz Paris La Défense Arena', 'Quiz Lazer', 'Quiz Stades en France', 'Quiz Saúde', 'Quiz Invalides & Pont d\'Iéna', 'Quiz Viagem', 'Quiz Arenas Paris Sud', 'Quiz Cidade', 'Quiz Ailleurs en France', 'Quiz Casa', 'Quiz La Concorde', 'Quiz Gastronomia', 'Quiz Arena Bercy', 'Quiz Moda', 'Quiz Grand Palais', 'Quiz DELF A1', 'Quiz Arena Porte de La Chapelle']
 
@@ -94,14 +79,17 @@ elif course_ids[0] == 'course-v1:af-brasil+PP+TB' :
 
 elif course_ids[0] == 'course-v1:af-brasil+OFM+01' :
   HEADERS_SECTION = ['Quiz Unité 1', 'Quiz Unité 2','Quiz Unité 3','Quiz Unité 4','Quiz Unité 5']
-else  :
+  UserGrade = ['QU1', 'QU2', 'QU3', 'QU4', 'QU5']
 
+else  :
   HEADERS_SECTION = ['Quiz Primeiros Passos', 'Quiz Destination Paris', 'Quiz Apresentações', 'Quiz Tour Eiffel & Champ de Mars', 'Quiz Família & Pets', 'Quiz Château de Versailles', 'Quiz Tempo', 'Quiz Stade de France', 'Quiz Festas & Tradições', 'Quiz Yvelines', 'Quiz Estudos', 'Quiz Seine-Saint-Denis', 'Quiz Trabalho', 'Quiz Paris La Défense Arena', 'Quiz Lazer', 'Quiz Stades en France', 'Quiz Saúde', 'Quiz Invalides & Pont d\'Iéna', 'Quiz Viagem', 'Quiz Arenas Paris Sud', 'Quiz Cidade', 'Quiz Ailleurs en France', 'Quiz Casa', 'Quiz La Concorde', 'Quiz Gastronomia', 'Quiz Arena Bercy', 'Quiz Moda', 'Quiz Grand Palais', 'Quiz DELF A1', 'Quiz Arena Porte de La Chapelle']
 
 
-
 HEADERS_USER.extend(HEADERS_SECTION)
+HEADERS_USER.append('Note globale (en %)')
 
+HEADERS_AFTER_SECTION = [u"Certificate date", u"Time tracking", u"Days logged"]
+HEADERS_USER.extend(HEADERS_AFTER_SECTION)
 
 HEADER = HEADERS_USER
 
@@ -136,46 +124,41 @@ for course_id in course_ids:
   for i in range(len(course_enrollments)):
     user = course_enrollments[i].user
     user_data = []
-
     enrollment = course_enrollments[i]
-
-    UserGrade = ['CFL1','DFL1','CFL2','DFL2','CFL3','DFL3','CFL4','DFL4','CFL5','DFL5','CFL6','DFL6','CFL7','DFL7','CFL8','DFL8','CFL9','DFL9','CFL10','DFL10','CFL11','DFL11','CFL12','DFL12','CFL13','DFL13','CFL14','DFL14','CFL15','DFL15']
+    
 
     user_CF_data = json.loads(user.profile.custom_field)
 
     # if str(user.email).find('@yopmail') != -1 or str(user.email).find('@weuplearning') != -1 or str(user.email).find('@themoocagency') != -1 :
     #   continue
 
-
-    user_data.append(user.id)
-
-    try:
-      user_data.append(user.email)
-    except:
-      try:
-        user_data.append(user_CF_data['email'])
-      except:
-        user_data.append('n.a.')
-    
     user_data.append(user.profile.name)
+    user_data.append(user.email)
+    user_data.append(user.username)
+
+    try :
+      certificate_date = user_CF_data["certificate_date_"+str(course_id.replace("-","_").replace(":","_").replace("+","_"))]
+    except :
+      certificate_date = 'n.a.'
 
 
     for key in TECHNICAL_HEADER :
-
       try :
         user_data.append(user_CF_data[key])
       except :
         user_data.append('n.a.')
 
 
+    user_data.append(user.date_joined.strftime('%d %b %y'))
+    user_data.append(user.last_login.strftime('%d %b %y'))
 
-    # Grade
-    # Il faut re-calculer la note pour prendre en compte l'historique des données 
+
+
+    # Grade  -  Il faut re-calculer la note pour prendre en compte l'historique des données 
     gradesTest = check_best_grade(user, course, force_best_grade=True)
 
     for section in gradesTest.summary['section_breakdown'] :
       UserGrade = [str(section['percent']) if grade == section['label'] else grade for grade in UserGrade]
-
 
 
     csv_user_grade = []
@@ -191,9 +174,7 @@ for course_id in course_ids:
             i+=1
 
           continue
-    
 
-    # Calculer la note globale à partir des notes agglomérées
     sumGrade = 0
     numGrade = 0
     for grade in UserGrade :
@@ -207,7 +188,21 @@ for course_id in course_ids:
       globalGradeStr = '0%'
 
 
-    data = { "general": user_data, "grade_section": UserGrade, "grade_global" :globalGradeStr }
+    #TimeTracking
+    try:
+      wul_course_enrollment = WulCourseEnrollment.objects.get(course_enrollment_edx__user=user, course_enrollment_edx__course_id=course_key)
+      global_time_tracking = wul_course_enrollment.global_time_tracking
+      days_logged = wul_course_enrollment.detailed_time_tracking.count(',')+1
+    except:
+      global_time_tracking = 0
+
+    time_tracking = []
+    time_tracking.append(global_time_tracking)
+    time_tracking.append(days_logged)
+
+
+
+    data = { "general": user_data, "grade_section": UserGrade, "grade_global" :globalGradeStr, "certificate_date": certificate_date, "time_tracking" : time_tracking }
 
     course_data[str(user.id)] = data
 
@@ -248,27 +243,29 @@ j=2
 for k, course_id in all_users_data.items():
   
   for key, user in course_id.items():
-    for i in range(len(user['general'])):
 
+    for i in range(len(user['general'])):
       if user['general'][i] in correspondance_CF:
         sheet.cell(j, i+1, correspondance_CF[user['general'][i]])
       else :
         sheet.cell(j, i+1, user['general'][i])
 
-    sheet.cell(j, 6, user['grade_global'])
-
     for i in range(len(user['grade_section'])):
-      sheet.cell(j, i+7, user['grade_section'][i])
+      sheet.cell(j, i+8, user['grade_section'][i])
+
+    sheet.cell(j, i+9, user['grade_global'])
+    sheet.cell(j, i+10, user['certificate_date'])
+
+    for i in range(len(user['time_tracking'])):
+      sheet.cell(j, i+40, user['time_tracking'][i])
 
     j += 1
 
 
 # SEND MAILS
-# course_names = []
 course_names_html = []
 for course_id in course_ids: 
   course = get_course_by_id(CourseLocator.from_string(course_id)) 
-  # course_names.append(course.display_name_with_default)
   course_names_html.append("<li>"+ str(course.display_name_with_default)+"</li>")
 
 output = BytesIO()
