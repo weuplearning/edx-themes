e3e9b15ef6860610bbed2334566571b106eadd77
theogicquel-weup <theo.gicquel@weuplearning.com>
2025-10-22 12:30:02 +0000

    20295 - refonte dashboard feedback client v1

--- lms/static/js/dashboard_trads.js
+++ lms/static/js/dashboard_trads.js
@@ -12,63 +12,58 @@ window.addEventListener("load", function () {
         return undefined;
     }
 
+
     let currentLanguage
     try{
         currentLanguage = getCookie('openedx-language-preference');
-    }
-    catch{
+    } catch{
         console.log("could not get openedx cookie")
     }
-    if(currentLanguage===undefined){
-        currentLanguage = getCookie('django_language');
+    if(currentLanguage === undefined){
+        try{
+            currentLanguage = getCookie('django_language');
+        } catch{
+            currentLanguage = "fr";
+            console.log("could not get openedx cookie")
+        }
     }
 
     function switchTrads(divId, frText, nlText) {
         console.log(divId)
         const targetDiv = document.getElementById(divId);       
         if (!targetDiv) return; 
-
         if (currentLanguage === "nl-nl" ) {
             targetDiv.innerText = nlText;
-            
         } else {
             targetDiv.innerText = frText;
         }
         
     }
 
-    function forceUpdating()
-    {
-        console.log("force")
-    switchTrads("nav-webinaire", "Webinaire BECOM", "BECOM-webinar"); 
-    switchTrads("nav-etudes", "Etudes", "Studies"); 
-    switchTrads("nav-actu", "Actualités", "Nieuws"); 
-    
-    switchTrads("footer_link_1", "Mentions légales", "Algemene voorwaarden"); 
-    switchTrads("footer_link_2", "Politique de confidentialité", "Privacybeleid"); 
+    function forceUpdating() {
+        // dashboard
+        switchTrads("title-part-1", "Qu'est ce que", "Wat is"); 
+        switchTrads("title-part-2", "la E-Academy ?", "de E-Academy ?"); 
+        switchTrads("descript-part-1", "Un programme de formation gratuit qui guide pas à pas les entrepreneurs belges dans leur transition numérique.", "Een gratis opleidingsprogramma dat Belgische ondernemers stap voor stap begeleidt bij hun digitale transitie.");
+        switchTrads("descript-part-2", "Vous y trouverez des contenus variés pour compléter votre panoplie d’outils et booster votre entreprise. Chaque cours, d’une durée moyenne de 40 minutes, vous donne les clés concrètes pour créer ou développer efficacement votre activité. Vous serez également informé en temps réel de nos évènements à venir !", "Ontdek praktische tools en inspirerende content om je bedrijf een boost te geven. Elke cursus, met een gemiddelde duur van 40 minuten, geeft u concrete tips om uw bedrijf op te zetten of effectief te ontwikkelen en blijf in realtime op de hoogte van  komende evenementen!");
+
+        // webinar    
+        switchTrads("webi-descript-title", "Retrouvez ici les enregistrements de nos évènements en ligne.", "Hier vindt u de opnames van onze online evenementen.");
+
+        // header
+        switchTrads("nav-webinaire", "Webinaires BECOM", "BECOM-webinars"); 
+        switchTrads("nav-etudes", "Etudes", "Studies"); 
+        switchTrads("nav-actu", "Actualités", "Nieuws"); 
+
+        // footer
+        switchTrads("footer_link_1", "Mentions légales", "Algemene voorwaarden"); 
+        switchTrads("footer_link_2", "Politique de confidentialité", "Privacybeleid"); 
     }
 
 
-
-        // header
     setTimeout(() => {
         forceUpdating()
-    }, 500); 
-
-
-
-
-    // dashboard
-    switchTrads("title-part-1", "Qu'est ce que", "Wat is"); 
-    switchTrads("title-part-2", "la E-Academy ?", "de E-Academy ?"); 
-    switchTrads("descript-part-1", "Un programme de formation gratuit qui guide pas à pas les entrepreneurs belges dans leur transition numérique.", "Een gratis opleidingsprogramma dat Belgische ondernemers stap voor stap begeleidt bij hun digitale transitie.");
-    switchTrads("descript-part-2", "Vous y trouverez des contenus variés pour compléter votre panoplie d’outils et booster votre entreprise. Vous serez également informé en temps réel de nos évènements à venir !", "Ontdek praktische tools en inspirerende content om je bedrijf een boost te geven, en blijf in realtime op de hoogte van  komende evenementen!");
-
-    // webinar    
-    switchTrads("webi-descript-title", "Retrouver ici les enregistrements de nos évènements en ligne.", "Hier vindt u de opnames van onze online evenementen.");
-    
-    // footer
-
+    }, 200);
 
 
 
@@ -93,78 +88,58 @@ window.addEventListener("load", function () {
 
     // Update footer links
     if (currentLanguage == "nl-nl"  ) {
-
         var link1 = document.getElementById("footer_link_1");
         var link2 = document.getElementById("footer_link_2");
-        var link3 = document.getElementById("footer_link_3");
-        var footerRight = document.getElementById("text-footer-right")
 
-        if (footerRight) {
-            footerRight.innerText = "Een programma op initiatief van"
-        }
-
-        // Change the href attributes
         try {
             link1.href = "/nl/wettelijke-vermeldingen/";
             link2.href = "/nl/beleid-inzake-gegevensbescherming/";
-            link3.href = "/nl/faq-nl/";
         } catch (error) {
-            console.log(error);
+            console.log('footer links not found');
+        }
+    }
+
+
+    setTimeout(() => {
+
+        try {
+            var ongoingCourses = document.getElementById('not-last-courses')
+
+            if (ongoingCourses.children.length == 0) {
+                if (currentLanguage === "nl-nl") {
+                    ongoingCourses.innerText = 'Deze lijst is momenteel leeg' 
+                } else {
+                    ongoingCourses.innerText = 'Cette liste est actuellement vide' 
+                }
+            }
+        } catch (error) {
+            console.log('no not-last-courses section');
         }
 
         try {
-            link1.innerHTML = "Algemene voorwaarden";
-
+            var finishedCourses = document.getElementById('finished-courses')
+            
+            if (finishedCourses.children.length == 0) {
+                if (currentLanguage === "nl-nl") {
+                    finishedCourses.innerText = 'Deze lijst is momenteel leeg' 
+                } else {
+                    finishedCourses.innerText = 'Cette liste est actuellement vide' 
+                }
+            }
         } catch (error) {
-            console.log(error);
-        }
-    }
-
-    
-})
-
-
-setTimeout(() => {
-
-    try {
-        var ongoingCourses = document.getElementById('not-last-courses')
-        
-        if (ongoingCourses.children.length == 0) {
-            ongoingCourses.innerText = 'Cette liste est actuellement vide' 
-        }
-        if (currentLanguage === "nl-nl") {
-            if (finishedCourses.children.length == 0) {
-                finishedCourses.innerText = 'Deze lijst is momenteel leeg' 
-            }
-        }
-        
-    } catch (error) {
-        console.log(error);
-    }
-
-    try {
-        var finishedCourses = document.getElementById('finished-courses')
-        if (finishedCourses.children.length == 0) {
-            finishedCourses.innerText = 'Cette liste est actuellement vide' 
-        }
-        if (currentLanguage === "nl-nl") {
-            if (finishedCourses.children.length == 0) {
-                finishedCourses.innerText = 'Deze lijst is momenteel leeg' 
-            }
+            console.log('no finished-courses section');
         }
 
-    } catch (error) {
-        console.log(error);
-    }
-
-}, 1000);
+    }, 500);
 
 
 
-setTimeout(() => {
-    var emptySection = document.getElementsByClassName('empty-dashboard-message')[0]
-    if (emptySection) {
-        emptySection.getElementsByClassName('btn')[0].href = '/dashboard/elearning'
-    }
-}, 1000);
+    setTimeout(() => {
+        var emptySection = document.getElementsByClassName('empty-dashboard-message')[0]
+        if (emptySection) {
+            emptySection.getElementsByClassName('btn')[0].href = '/dashboard/elearning'
+        }
+    }, 500);
+
+});
 
